ed9192c2feb7fd551f62e2404e82455ec27f8153
saulandresv <saul.vega2@gmail.com>
2025-07-14 01:21:32 -0400

    fix v3

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from "url";
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 ¡ESTO ES CLAVE!
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
